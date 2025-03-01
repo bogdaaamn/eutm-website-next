39a4b08ee0c056d78534a4649cb08cf04f46457f
Bogdan Covrig <bogcov@gmail.com>
2025-03-01 12:47:05 +0100

    refactor: Remove unused event detail page

--- src/app/event/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getMeetupEvent } from "~/clients/meetup";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-
-  const event = await getMeetupEvent(id);
-
-  return <pre>{JSON.stringify(event.data.event, null, 2)}</pre>;
-}
